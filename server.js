f2ca5c01a728b00253ec608c90b1a403f349a9f3
FIZZO PANNOSCH <fizzo999@gmail.com>
2021-03-03 17:01:46 -0800

    made it work again

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 // ============== Packages ==============================
 const express = require('express');
 const superagent = require('superagent');
@@ -6,51 +6,48 @@ const pg = require('pg');
 require('dotenv').config();
 
 // ============== App ===================================
-const app = express();
-const PORT = process.env.PORT;
-const DATABASE_URL = process.env.DATABASE_URL || 3111;
+const PORT = process.env.PORT || 3232;
+const DATABASE_URL = process.env.DATABASE_URL;
 const client = new pg.Client(DATABASE_URL);
 client.on('error', error => console.log('There was an error like dudh', error));
 
+const app = express();
 app.set('view engine', 'ejs');
-
-app.use(express.static('./public')); // serve all the files in the specified folder
 app.use(express.urlencoded({extended: true})); // tells express to peel off form data and put it into request.body
-app.use(express.static(__dirname + '/public'));
 
+app.use(express.static(__dirname + '/public'));
 // ============== Routes ================================
 app.get('/test', handleTest);
 app.get('/', getBooksSql);
-app.get('/books/:id', getSingleBook);
-
 app.get('/searches/new', (req, res) => {
-  res.render('/pages/searches/new.ejs');
+  res.render('pages/searches/new.ejs');
 });
-
 app.post('/searches', googleBookRequest);
+app.get('/books/:id', getSingleBook);
+app.post('/books', saveSingleBook);
 
-app.post('/books', (req, res) => {
+
+// =========== functions ============
+function saveSingleBook (req, res) {
   const sqlString2 = `INSERT INTO book_table (authors, title, isbn, image_url, book_description, pubDate) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id, authors, title, isbn, image_url, book_description, pubDate`;
   const sqlArray2 = [req.body.authors, req.body.title, req.body.isbn, req.body.image_url, req.body.book_description, req.body.pubDate];
   client.query(sqlString2, sqlArray2)
     .then(results => {
       const ejsObject3 = {bookSearchArray: req.body};
       res.redirect('/books/' + results.rows[0].id);
-      res.render('pages/books/detail.ejs', ejsObject3);
+      // res.render('pages/books/detail.ejs', ejsObject3);
     })
     .catch(errorThatComesBack => {
       console.log(errorThatComesBack);
       res.status(500).send('Sorry something went wrong with books ');
     });
-});
+}
 
 function getSingleBook (req, res) {
-  // console.log(req.params);
   const sqlString = 'SELECT * FROM book_table WHERE id = $1;';
-  const sqlArray = [req.params.id];
+  const sqlArray = [req.params.id]; //params gives you the parameter of what was in the url/
   client.query(sqlString, sqlArray)
     .then (result => {
-      // console.log('here is the database rows', result.rows);
       const ejsObject = { bookSearchArray: result.rows[0]};
       res.render('./pages/books/detail.ejs', ejsObject);
     })
@@ -61,11 +58,9 @@ function getSingleBook (req, res) {
 }
 
 function getBooksSql (req, res) {
-  // console.log(req.query);
   const sqlString = 'SELECT * FROM book_table;';
   client.query(sqlString)
     .then (result => {
-      // console.log('here is the database rows', result.rows);
       const sqlObject = { bookSearchArray: result.rows};
       res.render('./pages/index.ejs', sqlObject);
     })
@@ -80,20 +75,18 @@ function handleTest(req, res) {
 }
 
 function googleBookRequest(req, res) {
-  // console.log('Here is the REQU BODY ========',req.body);
-  let bookSearch = req.body.bookSearch;
+  let bookSearch = req.body.bookSearch; //because it is a POST method data shows up in req.body
   let search = req.body.search;
   let url = `https://www.googleapis.com/books/v1/volumes?q=in${search}:${bookSearch}`;
   superagent.get(url)
     .then(bookDataFromGoogle => {
-      // console.log('HERE IS THE GOOGLE FEEDBACK =======', bookDataFromGoogle.body);
       const bookSearchArray = bookDataFromGoogle.body.items.map(bookResults => new Books(bookResults));
       res.render('pages/searches/show.ejs', {bookSearchArray:bookSearchArray});
       // res.redirect('pages/searches/show.ejs');
     })
     .catch(errorThatComesBack => {
       console.log(errorThatComesBack);
-      res.status(500).send('Sorry something went wrong with books');
+      res.status(500).send('Sorry something went wrong with looking up books at google');
     });
 }
 
@@ -101,15 +94,15 @@ function Books(booksFromGoogle) {
   this.authors = booksFromGoogle.volumeInfo.authors;
   this.title = booksFromGoogle.volumeInfo.title;
   this.isbn = booksFromGoogle.volumeInfo.industryIdentifiers ? booksFromGoogle.volumeInfo.industryIdentifiers[1].type + booksFromGoogle.volumeInfo.industryIdentifiers[1].identifier : 'no ISBN number';
-  this.image_url = booksFromGoogle.volumeInfo.imageLinks ? booksFromGoogle.volumeInfo.imageLinks.smallThumbnail : "https://i.imgur.com/J5LVHEL.jpg";
+  this.image_url = booksFromGoogle.volumeInfo.imageLinks ? booksFromGoogle.volumeInfo.imageLinks.smallThumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
   this.book_description = booksFromGoogle.volumeInfo.description;
   this.pubDate = booksFromGoogle.volumeInfo.publishedDate;
 }
 
-
 // ============== Initialization ========================
 client.connect()
-.then(() => {
-app.listen(PORT, () => console.log(`up on http://localhost:${PORT}`));
-});
-
+  .then(() => {
+    app.listen(PORT, () => console.log(`up on http://localhost:${PORT}`));
+  }).catch(errorThatComesBack => {
+    console.log(errorThatComesBack);
+  });
